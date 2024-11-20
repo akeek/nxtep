7f0bb9a7fd299af454ce29c511ba1ef39c1183e6
Åke Ek <svein.ek@stud.noroff.no>
2024-11-20 17:29:12 +0100

    rydde opp

--- src/app/statsforvalteren/page.tsx
+++ src/app/statsforvalteren/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import CombinedData from "./combinedData";
-import PdfDataPage from "./pdfDataPage";
 
 const Statsforvalteren = () => {
   return (
     <div className="container mx-auto">
       <CombinedData />
-      {/* <PdfDataPage /> */}
     </div>
   );
 };
