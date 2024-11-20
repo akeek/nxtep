4099b3a5c647f71f58b76645ca15a7b3085036b8
Åke Ek <svein.ek@stud.noroff.no>
2024-11-20 19:48:05 +0100

    fikse p homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,17 @@ import { Button } from "../components/ui/button";
 
 const HomePage = () => {
   return (
-    <div className="container px-6">
+    <div className="container mx-auto">
       <h1 className="text-center text-5xl py-6">Welcome to NxtEp</h1>
-      <Button
-        variant="outline"
-        className="bg-green-50 hover:bg-green-500 hover:text-white font-semibold"
-        asChild
-      >
-        <Link href="/statsforvalteren">Statsforvalterens høringer</Link>
-      </Button>
+      <div className="px-6">
+        <Button
+          variant="outline"
+          className="bg-green-50 hover:bg-green-500 hover:text-white font-semibold"
+          asChild
+        >
+          <Link href="/statsforvalteren">Statsforvalterens høringer</Link>
+        </Button>
+      </div>
     </div>
   );
 };
